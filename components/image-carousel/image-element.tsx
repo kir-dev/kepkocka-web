061178632ed28a22c2be6c02c3a53c1594c416bd
FearsomeRover <bujgergo@gmail.com>
2025-03-12 17:53:57 +0100

    fixed image carousel

--- components/image-carousel/image-element.tsx
+++ components/image-carousel/image-element.tsx
@@ -1,20 +1,28 @@
-export default function ImageElement({ item }: { item: any }) {
+export default function ImageElement({
+  onClick,
+  item,
+  active,
+}: {
+  onClick: () => void;
+  item: any;
+  active: boolean;
+}) {
   return (
-    <div
-      className="w-dvw h-dvh bg-cover"
-      style={{
-        backgroundImage: `url(${item.image})`,
-      }}
-    >
-      <div className="inline-block m-6 w-64 pr-11 pl-2 border-l-2 cursor-pointer align-top text-left h-5 mt-10 overflow-hidden transition-all">
+    <button onClick={onClick}>
+      <div
+        className={`inline-block m-6 w-64 pr-11 pl-2 border-l-2 cursor-pointer align-top text-left overflow-hidden hover:h-16 hover:mt-5 transition-all text-sm text-white ${
+          active ? "mt-5 h-16" : "mt-10  h-6"
+        }`}
+      >
         <p>
-          <span className="title">{item.name}</span>
-          <br />
+          <span className="text-xl w-full text-center uppercase block">
+            {item.name}
+          </span>
           {item.subtitle}
           <br />
           {item.year}
         </p>
       </div>
-    </div>
+    </button>
   );
 }
